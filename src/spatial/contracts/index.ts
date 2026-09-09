14679a82667326a4a3190c3762fc48bbd109b568
Vijaya Kumar <84652701+parakkai99@users.noreply.github.com>
2026-09-10 03:28:36 +0530

    add spatial contract exports

---
+++ src/spatial/contracts/index.ts
@@ -0,0 +1,19 @@
+/**
+ * Architect: Vijay Kumar K.
+ * Platform: ArchitectAny (AAi)
+ * Contract: AAi Generic Spatial Framework
+ * Status: ACTIVE
+ *
+ * Public contract exports for the generic Spatial layer.
+ */
+
+export type {
+  SpatialAnimationReference,
+  SpatialFocalPoint,
+  SpatialLayerDefinition,
+  SpatialLayerRole,
+  SpatialObjectDefinition,
+  SpatialSceneAsset,
+  SpatialSceneDefinition,
+  SpatialSceneKind,
+} from "./SpatialSceneDefinition";
