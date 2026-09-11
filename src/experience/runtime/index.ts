b442b325939caa791155108752e69096303c7b72
Vijaya Kumar <84652701+parakkai99@users.noreply.github.com>
2026-09-11 14:10:07 +0530

    add experience runtime exports

---
+++ src/experience/runtime/index.ts
@@ -0,0 +1,10 @@
+/**
+ * AAi Experience Runtime Public Surface
+ * Architect: Vijay Kumar K.
+ * Platform: ArchitectAny (AAi)
+ * Contract: EXPERIENCE-RUNTIME-001
+ * Status: ACTIVE
+ */
+
+export { ExperienceShell } from "./ExperienceShell";
+export type { ExperienceShellProps } from "./ExperienceShell";
