460f0659b8abf7f9e6f7c34421a3e6864985c05d
Vijaya Kumar <84652701+parakkai99@users.noreply.github.com>
2026-09-11 13:47:16 +0530

    add layered experience definition index.ts

---
+++ src/experience/resolver/index.ts
@@ -0,0 +1,9 @@
+/**
+ * AAi Experience Resolver Public Surface
+ * Architect: Vijay Kumar K.
+ * Platform: ArchitectAny (AAi)
+ * Contract: EXPERIENCE-DEFINITION-001
+ * Status: ACTIVE
+ */
+
+export { resolveExperienceDefinition } from "./ExperienceDefinitionResolver";
