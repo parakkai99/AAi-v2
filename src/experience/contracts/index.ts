56b38eb64b5a15fbc56eb5a4356973f266a27e16
Vijaya Kumar <84652701+parakkai99@users.noreply.github.com>
2026-09-11 13:47:10 +0530

    add layered experience definition index.ts

---
+++ src/experience/contracts/index.ts
@@ -0,0 +1,19 @@
+/**
+ * AAi Experience Contracts Public Surface
+ * Architect: Vijay Kumar K.
+ * Platform: ArchitectAny (AAi)
+ * Contract: EXPERIENCE-DEFINITION-001
+ * Status: ACTIVE
+ */
+
+export type {
+  ExperienceAppStackDefinition,
+  ExperienceBehaviorSurfaceDefinition,
+  ExperienceContextDefinition,
+  ExperienceDefinition,
+  ExperienceIdentityDefinition,
+  ExperienceInfrastructureDefinition,
+  ExperienceNavigationBehaviorDefinition,
+  ExperienceScope,
+  ExperienceSearchDefinition,
+} from "./ExperienceDefinition";
