d912d4d396677e00e1976a7cd9fc7e969b87ba3b
Vijaya Kumar <84652701+parakkai99@users.noreply.github.com>
2026-09-15 18:44:55 +0530

    initialize ArchitectAny Capacitor configuration

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'in.architectany.aai',
+  appName: 'ArchitectAny',
+  webDir: 'dist'
+};
+
+export default config;
